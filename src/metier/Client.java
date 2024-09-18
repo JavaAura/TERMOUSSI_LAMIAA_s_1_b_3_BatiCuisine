3c2248f01b6055c68284e4ccef365bdd35438998
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-09-18 17:04:25 +0100

    client class

---
+++ src/metier/Client.java
@@ -0,0 +1,72 @@
+package metier;
+
+public class Client {
+	
+	private int id;
+    private String nom;
+    private String adresse;
+    private String email;
+    private String telephone;
+
+    public Client() {
+    }
+
+    public Client(int id, String nom, String adresse, String email, String telephone) {
+        this.id = id;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.email = email;
+        this.telephone = telephone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
+}
---
+++ src/presentation/Main.java
@@ -0,0 +1,14 @@
+package presentation;
+
+import java.sql.SQLException;
+
+import database.DbConnection;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	
+	}
+
+}
