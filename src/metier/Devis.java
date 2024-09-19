54b3175edc1d51a8707362380c93d25eac3f5468
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-09-19 10:00:12 +0100

    Devis class

---
+++ src/metier/Devis.java
@@ -0,0 +1,82 @@
+package metier;
+
+import java.time.LocalDate;
+
+public class Devis {
+	
+	 	private int id;
+	    private double montantEstime;  
+	    private LocalDate dateEmission;  
+	    private LocalDate dateValidite; 
+	    private boolean accepte;        
+	    private Projet projet;        
+
+	    public Devis(int id, double montantEstime, LocalDate dateEmission, LocalDate dateValidite, boolean accepte, Projet projet) {
+	        this.id = id;
+	        this.montantEstime = montantEstime;
+	        this.dateEmission = dateEmission;
+	        this.dateValidite = dateValidite;
+	        this.accepte = accepte;
+	        this.projet = projet;
+	    }
+	    
+	    public int getId() {
+	        return id;
+	    }
+
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+
+	    public double getMontantEstime() {
+	        return montantEstime;
+	    }
+
+	    public void setMontantEstime(double montantEstime) {
+	        this.montantEstime = montantEstime;
+	    }
+
+	    public LocalDate getDateEmission() {
+	        return dateEmission;
+	    }
+
+	    public void setDateEmission(LocalDate dateEmission) {
+	        this.dateEmission = dateEmission;
+	    }
+
+	    public LocalDate getDateValidite() {
+	        return dateValidite;
+	    }
+
+	    public void setDateValidite(LocalDate dateValidite) {
+	        this.dateValidite = dateValidite;
+	    }
+
+	    public boolean isAccepte() {
+	        return accepte;
+	    }
+
+	    public void setAccepte(boolean accepte) {
+	        this.accepte = accepte;
+	    }
+
+	    public Projet getProjet() {
+	        return projet;
+	    }
+
+	    public void setProjet(Projet projet) {
+	        this.projet = projet;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Devis{" +
+	                "id=" + id +
+	                ", montantEstime=" + montantEstime +
+	                ", dateEmission=" + dateEmission +
+	                ", dateValidite=" + dateValidite +
+	                ", accepte=" + accepte +
+	                ", projet=" + projet +
+	                '}';
+	    }
+}
